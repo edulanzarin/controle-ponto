9a7e2253eccdbe61f5a1ffdb1386d9fb843b4215
edulanzarin <edulanzarin@outlook.com>
2025-06-18 20:23:00 -0300

    feat: criado modelo Registro para representar ponto e TipoRegistro para representar tipo do registro

---
+++ src/model/Registro.java
@@ -0,0 +1,68 @@
+package model;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.UUID;
+
+/*
+ * objeto Registro com a estrutura necessária para armazenar o ponto no arquivo .csv
+ * 
+ * id               id exclusivo gerado automaticamente
+ * tipoRegistro     tipo de registro: ENTRADA, INICIO_INTERVALO, FIM_INTERVALO, SAIDA, OUTRO
+ * data             data do registro
+ * hora             hora do registro
+ * observacao       alguma observacao sobre esse registro
+ */
+public class Registro {
+    
+    private String id = UUID.randomUUID().toString();
+    private TipoRegistro tipoRegistro;
+    private LocalDate data;
+    private LocalTime hora;
+    private String observacao;
+
+    /*
+     * não recebe id, pois está sendo gerado aleatoriamente com UUID.randomUUID()
+     * também não recebe observacao, pois é uma informação opcional
+     */
+    public Registro(TipoRegistro tipoRegistro, LocalDate data, LocalTime hora) {
+        this.tipoRegistro = tipoRegistro;
+        this.data = data;
+        this.hora = hora;
+    }
+
+    public String getId() {
+        return id;
+    }
+    
+    public TipoRegistro getTipoRegistro() {
+        return tipoRegistro;
+    }
+    public void setTipoRegistro(TipoRegistro tipoRegistro) {
+        this.tipoRegistro = tipoRegistro;
+    }
+    public LocalDate getData() {
+        return data;
+    }
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+    public LocalTime getHora() {
+        return hora;
+    }
+    public void setHora(LocalTime hora) {
+        this.hora = hora;
+    }
+    public String getObservacao() {
+        return observacao;
+    }
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Registro [id=" + id + ", tipoRegistro=" + tipoRegistro + ", data=" + data + ", hora=" + hora
+                + ", observacao=" + observacao + "]";
+    }
+}
---
+++ src/model/TipoRegistro.java
@@ -0,0 +1,9 @@
+package model;
+
+public enum TipoRegistro {
+    ENTRADA,
+    INICIO_INTERVALO,
+    FIM_INTERVALO,
+    SAIDA,
+    OUTRO
+}
