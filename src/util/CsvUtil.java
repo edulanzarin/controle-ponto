6a56c498915ee002e380b040326e383055ceac23
edulanzarin <edulanzarin@outlook.com>
2025-06-18 23:51:47 -0300

    chore: cria arquivo CsvUtil.java com métodos stub para ler, escrever e salvar CSV

--- src/util/CsvUtil.java
+++ src/util/CsvUtil.java
@@ -1,5 +1,22 @@
 package util;
 
+import java.util.List;
+
 public class CsvUtil {
     
+    public List<String[]> lerCsv(String caminhoCsv) {
+        /* TODO implementar método para ler registros de ponto do .csv */
+        return List.of();
+    }
+
+    public void escreverCsv(String caminhoCsv, String dados) {
+        /* TODO implementar método para inserir registros de ponto no .csv */
+    }
+
+    public void salvarCsv(String caminhoCsv) {
+        /*
+         * TODO implementar método para salvar arquivo .csv dependendo do SO
+         * permite salvamento em sistemas Linux e Windows
+         */
+    }
 }
